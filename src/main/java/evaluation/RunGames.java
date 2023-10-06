88e8f142d5ed9df327286afae01f5fe25f615c39
Goutham <Gouthamsai5059@gmail.com>
2023-10-06 13:03:50 +0100

    dvusdv

--- src/main/java/evaluation/RunGames.java
+++ src/main/java/evaluation/RunGames.java
@@ -87,7 +87,7 @@ public class RunGames implements IGameRunner {
             agents.addAll(PlayerFactory.createPlayers((String) runGames.config.get(playerDirectory)));
         } else {
             agents.add(new MCTSPlayer());
-//            agents.add(new BasicMCTSPlayer());
+            agents.add(new BasicMCTSPlayer());
             agents.add(new RandomPlayer());
             agents.add(new RMHCPlayer());
             agents.add(new OSLAPlayer());
